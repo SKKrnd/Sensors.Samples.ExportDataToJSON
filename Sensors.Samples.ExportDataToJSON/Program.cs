7cc2f4ab9cc35ddad4cc39a5c3436ccdcb36ee7c
SKK\michal.august <michal.august@skkglobal.com>
2019-01-07 15:08:15 +0100

    Init

---
+++ Sensors.Samples.ExportDataToJSON/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Sensors.Samples.ExportDataToJSON
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Tokens.ClientId = "<clientId>";
+            Tokens.ClientSecret = "<clientSecret>";
+            //
+            Console.WriteLine("Please pass orderId :");
+            var orderId = Console.ReadLine();
+            //
+            Console.WriteLine("Please pass orderUnitId :");
+            var orderUnitId = Console.ReadLine();
+            //
+            var outputFilePath = @"D:\OrderUnitReadings.json";
+            if (File.Exists(outputFilePath))
+                File.Delete(outputFilePath);
+            //
+
+            // Requesting AccessToken. It is needed for a parameter given to the uri that gives back json
+            var token = new Tokens();
+            var tokenResponse = await token.RequestTokenAsync();
+            
+            // Uri contains of orderID and orderUnitID given given as a parameter to program and user's accesstoken
+            var uri = new Uri($"https://skk-sensors-functions-pro.azurewebsites.net/api/ExportOrderUnitDataToJSON" +
+                $"?orderId={orderId}" +
+                $"&orderUnitId={orderUnitId}" +
+                $"&accessToken={tokenResponse.AccessToken}");
+            
+            //Acquiring and saving json
+            using (var w = new WebClient())
+            {
+                try
+                {
+                    var downloadedObject = w.DownloadString(uri);
+                    //                    
+                    using (var writer = new StreamWriter(outputFilePath, false))
+                        writer.Write(downloadedObject);
+                    //
+                    Console.WriteLine($"Json data saved : {outputFilePath}.");
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Sensors.Samples.ExportDataToJSON/Tokens.cs
@@ -0,0 +1,35 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensors.Samples.ExportDataToJSON
+{
+    public class Tokens
+    {
+        public async Task<TokenResponse> RequestTokenAsync()
+        {
+            var client = new HttpClient();
+            //
+            return await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = "https://identity.skkhive.com/connect/token",
+                //
+                ClientId = ClientId 
+                    ?? throw new ArgumentNullException(nameof(Tokens.ClientId)),
+                ClientSecret = ClientSecret
+                    ?? throw new ArgumentNullException(nameof(Tokens.ClientSecret)),
+                //
+                Parameters =
+                {
+                    { "scope", "sensors" }
+                }
+            });
+        }
+
+        public static string ClientId = null;
+        public static string ClientSecret = null;
+    }
+}
